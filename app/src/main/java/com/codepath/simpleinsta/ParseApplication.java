1396f3bf2e31ea3e81c77d247c8df5aec8028cad
Isha Kabra <isha.kabra10@gmail.com>
2019-02-16 02:08:26 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/simpleinsta/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.codepath.simpleinsta;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("isha-instagram") // should correspond to APP_ID env variable
+                .clientKey("CodePathInstagramMasterKey")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://isha-instagram.herokuapp.com/parse").build());
+    }
+}
