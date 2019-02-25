80715f02c8c0defdecad3a0ffc34a208b2f01be2
Isha Kabra <isha.kabra10@gmail.com>
2019-02-24 22:48:42 -0500

    Implemented pull-to-refresh feature

---
+++ app/src/main/java/com/codepath/simpleinsta/fragments/PostsFragment.java
@@ -0,0 +1,101 @@
+package com.codepath.simpleinsta.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.simpleinsta.Post;
+import com.codepath.simpleinsta.PostsAdapter;
+import com.codepath.simpleinsta.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostsFragment extends Fragment {
+
+    private RecyclerView rvPosts;
+    public static final String TAG = "PostsFragment";
+    protected PostsAdapter adapter;
+    protected List<Post> mPosts;
+
+    protected SwipeRefreshLayout swipeContainer;
+
+
+    //onCreateView to inflate the view
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        queryPosts();
+
+        // Lookup the swipe container view
+        swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+        // create the data source
+        mPosts = new ArrayList<>();
+        // create the adapter
+        adapter = new PostsAdapter(getContext(), mPosts);
+        // set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        // set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Refresh the list
+                queryPosts();
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    protected void queryPosts() {
+        ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
+        postQuery.include(Post.KEY_USER);
+        postQuery.setLimit(20);
+        postQuery.addDescendingOrder(Post.KEY_CREATED_AT);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error with query");
+                    return;
+                }
+                adapter.clear();
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                for (int i = 0; i < posts.size(); i++) {
+                    Log.d(TAG,"Post: " + posts.get(i).getDescription());
+                }
+                // Call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+}
